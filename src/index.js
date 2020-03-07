b1f360818d2bcf19f76e1bb455e3bf4df78b7838
AmyFoxFN <funanamy@gmail.com>
2020-03-07 17:59:43 +0800

    feat: test imported case

--- src/index.js
+++ src/index.js
@@ -3,7 +3,15 @@ const protoBuf = require('protobufjs')
 main()
 
 async function main() {
-  const root = await protoBuf.load('src/protos/awesome.proto')
-  AwesomeMessage = root.lookupType("awesomepackage.AwesomeMessage")
-  console.log(111, AwesomeMessage)
+  const root = await protoBuf.load('src/protos/basic.proto')
+  BasicMessage = root.lookupType("awesomepackage.AwesomeMessage")
+  console.log('BasicMessage:', BasicMessage)
+
+  const rootImport = await protoBuf.load('src/protos/imported.proto')
+  console.log('ImportedRoot:', rootImport)
+
+  const commonMessage = rootImport.lookupType("common.Item")
+  const importedMessage = rootImport.lookupType("imported.Imported")
+  console.log('commonMessage:', commonMessage)
+  console.log('importedMessage:', importedMessage)
 }
