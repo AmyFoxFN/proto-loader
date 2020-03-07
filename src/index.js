4189f0c4e8415a44ca6432855b8ae130f48aff11
AmyFoxFN <funanamy@gmail.com>
2020-03-07 21:08:19 +0800

    parse pose

--- src/index.js
+++ src/index.js
@@ -3,20 +3,38 @@ const protoBuf = require('protobufjs')
 main()
 
 async function main() {
+  // parseBasicProto()
+  // parseImportProto('src/protos/imported.proto')
+  // test: out side directory
+  // parseImportProto('protos/imported.proto')
+
+  parsePoseProto()
+}
+
+async function parseBasicProto() {
   const root = await protoBuf.load('src/protos/basic.proto')
   BasicMessage = root.lookupType("awesomepackage.AwesomeMessage")
   console.log('BasicMessage:', BasicMessage)
-
-  // parseImportProto('src/protos/imported.proto')
-  parseImportProto('protos/imported.proto')
 }
 
 async function parseImportProto(path) {
-  const rootImport = await protoBuf.load(path)
-  console.log('ImportedRoot:', rootImport)
+  const root = await protoBuf.load(path)
+  console.log('root:', root)
 
-  const commonMessage = rootImport.lookupType("common.Common")
-  const importMessage = rootImport.lookupType("import.Import")
+  const commonMessage = root.lookupType("common.Common")
+  const importMessage = root.lookupType("import.Import")
   console.log('commonMessage:', commonMessage)
   console.log('importedMessage:', importMessage)
 }
+
+async function parsePoseProto() {
+  const root = await protoBuf.load('private-protos/pose.proto')
+  console.log('root:', root)
+
+  // const commonMessage = rootImport.lookupType("common.Common")
+  // const importMessage = rootImport.lookupType("import.Import")
+  // console.log('commonMessage:', commonMessage)
+  // console.log('importedMessage:', importMessage)
+}
+
+
